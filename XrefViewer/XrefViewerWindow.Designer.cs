e61abd2bc8141df414048e96c06ede1150d499ac
DragonPlayerX <dragon.player@outlook.de>
2021-12-07 08:03:07 +0100

    Improved design slightly, enabled window resize

--- XrefViewer/XrefViewerWindow.Designer.cs
+++ XrefViewer/XrefViewerWindow.Designer.cs
@@ -31,20 +31,20 @@ namespace XrefViewer
         {
             this.InputTextBox = new System.Windows.Forms.TextBox();
             this.ConsoleTextBox = new System.Windows.Forms.RichTextBox();
+            this.Container = new System.Windows.Forms.Panel();
+            this.Container.SuspendLayout();
             this.SuspendLayout();
             // 
             // InputTextBox
             // 
-            this.InputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.InputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.InputTextBox.Font = new System.Drawing.Font("Consolas", 12F);
             this.InputTextBox.ForeColor = System.Drawing.Color.White;
-            this.InputTextBox.Location = new System.Drawing.Point(12, 523);
+            this.InputTextBox.Location = new System.Drawing.Point(0, 535);
             this.InputTextBox.Name = "InputTextBox";
-            this.InputTextBox.Size = new System.Drawing.Size(960, 26);
+            this.InputTextBox.Size = new System.Drawing.Size(984, 26);
             this.InputTextBox.TabIndex = 0;
             this.InputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputTextBox_KeyPress);
             // 
@@ -58,14 +58,24 @@ namespace XrefViewer
             this.ConsoleTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ConsoleTextBox.ForeColor = System.Drawing.Color.White;
             this.ConsoleTextBox.HideSelection = false;
-            this.ConsoleTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ConsoleTextBox.Location = new System.Drawing.Point(0, 0);
             this.ConsoleTextBox.Margin = new System.Windows.Forms.Padding(0);
             this.ConsoleTextBox.Name = "ConsoleTextBox";
             this.ConsoleTextBox.ReadOnly = true;
             this.ConsoleTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.ConsoleTextBox.Size = new System.Drawing.Size(960, 505);
+            this.ConsoleTextBox.Size = new System.Drawing.Size(984, 532);
             this.ConsoleTextBox.TabIndex = 0;
             this.ConsoleTextBox.Text = "";
+            // 
+            // Container
+            // 
+            this.Container.Controls.Add(this.ConsoleTextBox);
+            this.Container.Controls.Add(this.InputTextBox);
+            this.Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Container.Location = new System.Drawing.Point(0, 0);
+            this.Container.Name = "Container";
+            this.Container.Size = new System.Drawing.Size(984, 561);
+            this.Container.TabIndex = 1;
             // 
             // XrefViewerWindow
             // 
@@ -73,22 +83,19 @@ namespace XrefViewer
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(984, 561);
-            this.Controls.Add(this.ConsoleTextBox);
-            this.Controls.Add(this.InputTextBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1000, 600);
-            this.MinimizeBox = false;
+            this.Controls.Add(this.Container);
             this.MinimumSize = new System.Drawing.Size(1000, 600);
             this.Name = "XrefViewerWindow";
             this.Text = "XrefViewer";
+            this.Container.ResumeLayout(false);
+            this.Container.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.TextBox InputTextBox;
         private System.Windows.Forms.RichTextBox ConsoleTextBox;
+        private System.Windows.Forms.Panel Container;
     }
 }
